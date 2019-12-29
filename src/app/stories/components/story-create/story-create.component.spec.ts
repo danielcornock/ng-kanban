90df62ff99d16443832c5113a8a2d34fcd78bfe7
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2019-12-29 16:10:26 +0000

    test(story-create): Add testing for story creation component

--- src/app/stories/components/story-create/story-create.component.spec.ts
+++ src/app/stories/components/story-create/story-create.component.spec.ts
@@ -1,25 +1,72 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { StoryCreateComponent } from './story-create.component';
+import { StoryCreateComponent } from "./story-create.component";
+import { FormBuilder } from "@angular/forms";
+import { StoryApiServiceStub } from "../../services/story-api.service.stub";
+import { StoryApiService } from "../../services/story-api.service";
+import { SharedModule } from "src/app/shared/shared.module";
+import { DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
-describe('StoryCreateComponent', () => {
+describe("StoryCreateComponent", () => {
   let component: StoryCreateComponent;
   let fixture: ComponentFixture<StoryCreateComponent>;
+  let dependencies: {
+    formBuilder: FormBuilder;
+    storyApiService: StoryApiServiceStub;
+  };
+
+  function getSubmitButton(): DebugElement {
+    return fixture.debugElement.query(By.css(".storyCreate-submit"));
+  }
 
   beforeEach(async(() => {
+    dependencies = {
+      formBuilder: new FormBuilder(),
+      storyApiService: new StoryApiServiceStub()
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ StoryCreateComponent ]
-    })
-    .compileComponents();
+      imports: [SharedModule],
+      declarations: [StoryCreateComponent],
+      providers: [
+        { provide: FormBuilder, useValue: dependencies.formBuilder },
+        { provide: StoryApiService, useValue: dependencies.storyApiService }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
+    const storyForm = dependencies.formBuilder.group({
+      title: ""
+    });
+    spyOn(dependencies.formBuilder, "group").and.returnValue(storyForm);
+
     fixture = TestBed.createComponent(StoryCreateComponent);
     component = fixture.componentInstance;
+    component.appStoryCreateColumnId = "columnId";
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe("on initialisation", () => {
+    it("should initialise the form", () => {
+      expect(dependencies.formBuilder.group).toHaveBeenCalledWith({
+        title: ["", jasmine.any(Function)]
+      });
+    });
+  });
+
+  describe("when submitting the form", () => {
+    beforeEach(() => {
+      component.storyForm.value.title = "test-title";
+      getSubmitButton().nativeElement.click();
+    });
+
+    it("should send the story to the API", () => {
+      expect(dependencies.storyApiService.addNewStory).toHaveBeenCalledWith(
+        "test-title",
+        "columnId"
+      );
+    });
   });
 });
---
+++ src/app/stories/components/story-create/story-create.component.stub.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: "app-story-create",
+  template: ""
+})
+export class StoryCreateComponentStub {
+  @Input() appStoryCreateColumnId: string;
+}
