7a9cdc6018eea8e5e3537280051017fe8aa56e6e
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-01-08 00:07:43 +0000

    test(story): Add unit tests for story

--- src/app/stories/components/story/story.component.spec.ts
+++ src/app/stories/components/story/story.component.spec.ts
@@ -1,25 +1,40 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { StoryComponent } from './story.component';
+import { StoryComponent } from "./story.component";
+import { IStory } from "../../interfaces/story.interface";
+import { DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
-describe('StoryComponent', () => {
+describe("StoryComponent", () => {
   let component: StoryComponent;
   let fixture: ComponentFixture<StoryComponent>;
 
+  function getTitle(): DebugElement {
+    return fixture.debugElement.query(By.css(".story-title"));
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ StoryComponent ]
-    })
-    .compileComponents();
+      declarations: [StoryComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(StoryComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe("on initialisation", () => {
+    beforeEach(() => {
+      component.appStory = {
+        title: "story-title"
+      } as IStory;
+
+      fixture.detectChanges();
+    });
+
+    it("should render the title", () => {
+      expect(getTitle().nativeElement.innerText).toBe("story-title");
+    });
   });
 });
