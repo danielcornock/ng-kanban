890a2df1672de853fc3f4f0af7d0153fda297cc3
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-01-09 23:48:00 +0000

    text(column-create): Write unit tests for column create component

--- src/app/boards/components/column-create/column-create.component.spec.ts
+++ src/app/boards/components/column-create/column-create.component.spec.ts
@@ -2,25 +2,78 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { ColumnCreateComponent } from "./column-create.component";
 import { SharedModule } from "src/app/shared/shared.module";
+import { FormBuilder } from "@angular/forms";
+import { HttpServiceStub } from "src/app/shared/api/http-service/http.service.stub";
+import { HttpService } from "src/app/shared/api/http-service/http.service";
+import { DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
 describe("ColumnCreateComponent", () => {
   let component: ColumnCreateComponent;
   let fixture: ComponentFixture<ColumnCreateComponent>;
+  let dependencies: {
+    formBuilder: FormBuilder;
+    httpService: HttpServiceStub;
+  };
+
+  function getSubmitButton(): DebugElement {
+    return fixture.debugElement.query(By.css(".columnCreate-submit"));
+  }
 
   beforeEach(async(() => {
+    dependencies = {
+      formBuilder: new FormBuilder(),
+      httpService: new HttpServiceStub()
+    };
+
     TestBed.configureTestingModule({
       imports: [SharedModule],
+      providers: [
+        { provide: FormBuilder, useValue: dependencies.formBuilder },
+        { provide: HttpService, useValue: dependencies.httpService }
+      ],
       declarations: [ColumnCreateComponent]
     }).compileComponents();
   }));
 
   beforeEach(() => {
+    const storyForm = dependencies.formBuilder.group({
+      title: ""
+    });
+    spyOn(dependencies.formBuilder, "group").and.returnValue(storyForm);
+
     fixture = TestBed.createComponent(ColumnCreateComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it("should create", () => {
-    expect(component).toBeTruthy();
+  describe("on initialisation", () => {
+    beforeEach(() => {
+      fixture.detectChanges();
+    });
+
+    it("should initialise the form", () => {
+      expect(dependencies.formBuilder.group).toHaveBeenCalledWith({
+        title: ["", jasmine.any(Function)]
+      });
+    });
+
+    describe("when the form is submitted", () => {
+      beforeEach(() => {
+        component.columnForm.value.title = "test-title";
+        spyOn(component.appColumnCreateOnCreate, "emit");
+        spyOn(component.columnForm, "reset");
+        getSubmitButton().nativeElement.click();
+      });
+
+      it("should emit the form value title", () => {
+        expect(component.appColumnCreateOnCreate.emit).toHaveBeenCalledWith(
+          "test-title"
+        );
+      });
+
+      it("should reset the form values", () => {
+        expect(component.columnForm.reset).toHaveBeenCalledWith();
+      });
+    });
   });
 });
