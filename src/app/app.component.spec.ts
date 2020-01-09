4626f84f89e6f8662405e25e9698eb86980ea3b3
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-01-09 00:38:25 +0000

    test(app): Add testing for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,71 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async, ComponentFixture } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { AppComponent } from "./app.component";
+import { AuthService } from "./authentication/services/auth/auth.service";
+import { AuthServiceStub } from "./authentication/services/auth/auth.service.stub";
+import { HeaderComponentStub } from "./shared/layouts/header/header.component.stub";
+import { DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
+  let fixture: ComponentFixture<AppComponent>,
+    dependencies: {
+      authService: AuthServiceStub;
+    };
+
+  function getHeader(): DebugElement {
+    return fixture.debugElement.query(By.directive(HeaderComponentStub));
+  }
+
   beforeEach(async(() => {
+    dependencies = {
+      authService: new AuthServiceStub()
+    };
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      providers: [{ provide: AuthService, useValue: dependencies.authService }],
+      declarations: [AppComponent, HeaderComponentStub]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
   });
 
-  it(`should have as title 'ng-kanban'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-kanban');
+  describe("on initialisation", () => {
+    beforeEach(() => {
+      fixture.detectChanges();
+    });
+
+    it("should check if the user is authenticated", () => {
+      expect(dependencies.authService.isAuthenticated).toHaveBeenCalledWith();
+    });
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-kanban!');
+  describe("when the user is not authenticated", () => {
+    beforeEach(() => {
+      (dependencies.authService.isAuthenticated as jasmine.Spy).and.returnValue(
+        false
+      );
+
+      fixture.detectChanges();
+    });
+
+    it("should not display the header", () => {
+      expect(getHeader() === null).toBe(true);
+    });
+  });
+
+  describe("when the user is authenticated", () => {
+    beforeEach(() => {
+      (dependencies.authService.isAuthenticated as jasmine.Spy).and.returnValue(
+        true
+      );
+      fixture.detectChanges();
+    });
+
+    it("should display the header", () => {
+      expect(getHeader() === null).toBe(false);
+    });
   });
 });
