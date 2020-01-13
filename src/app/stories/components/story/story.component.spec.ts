9c736807a838b7a479a58e9d99be7a1c01c7e547
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-01-13 21:34:53 +0000

    feat(story): Add open story modal button to story component

--- src/app/stories/components/story/story.component.spec.ts
+++ src/app/stories/components/story/story.component.spec.ts
@@ -4,18 +4,35 @@ import { StoryComponent } from "./story.component";
 import { IStory } from "../../interfaces/story.interface";
 import { DebugElement } from "@angular/core";
 import { By } from "@angular/platform-browser";
+import { ModalServiceStub } from "src/app/shared/modal/modal-service/modal.service.stub";
+import { ModalService } from "src/app/shared/modal/modal-service/modal.service";
+import { EditStoryModalComponent } from "../edit-story-modal/edit-story-modal.component";
 
 describe("StoryComponent", () => {
   let component: StoryComponent;
   let fixture: ComponentFixture<StoryComponent>;
+  let dependencies: {
+    modalService: ModalServiceStub;
+  };
 
   function getTitle(): DebugElement {
     return fixture.debugElement.query(By.css(".story-title"));
   }
 
+  function getOpenModalButton(): DebugElement {
+    return fixture.debugElement.query(By.css(".story-container"));
+  }
+
   beforeEach(async(() => {
+    dependencies = {
+      modalService: new ModalServiceStub()
+    };
+
     TestBed.configureTestingModule({
-      declarations: [StoryComponent]
+      declarations: [StoryComponent],
+      providers: [
+        { provide: ModalService, useValue: dependencies.modalService }
+      ]
     }).compileComponents();
   }));
 
@@ -27,7 +44,9 @@ describe("StoryComponent", () => {
   describe("on initialisation", () => {
     beforeEach(() => {
       component.appStory = {
-        title: "story-title"
+        title: "story-title",
+        _id: "story-id",
+        storyNumber: 5
       } as IStory;
 
       fixture.detectChanges();
@@ -35,6 +54,29 @@ describe("StoryComponent", () => {
 
     it("should render the title", () => {
       expect(getTitle().nativeElement.innerText).toBe("story-title");
+    });
+
+    it("should render the story number", () => {
+      const storyNumber = fixture.debugElement.query(
+        By.css(".story-storyNumber")
+      );
+
+      expect(storyNumber.nativeElement.innerText).toBe("#5");
+    });
+
+    describe("when the story is clicked on", () => {
+      beforeEach(() => {
+        getOpenModalButton().nativeElement.click();
+      });
+
+      it("should call the service to open the edit story modal", () => {
+        expect(dependencies.modalService.openModal).toHaveBeenCalledWith(
+          EditStoryModalComponent,
+          {
+            storyId: "story-id"
+          }
+        );
+      });
     });
   });
 });
--- src/app/stories/components/story/story.component.ts
+++ src/app/stories/components/story/story.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from "@angular/core";
 import { IStory } from "../../interfaces/story.interface";
+import { ModalService } from "src/app/shared/modal/modal-service/modal.service";
+import { EditStoryModalComponent } from "../edit-story-modal/edit-story-modal.component";
 
 @Component({
   selector: "app-story",
@@ -8,7 +10,14 @@ import { IStory } from "../../interfaces/story.interface";
 })
 export class StoryComponent implements OnInit {
   @Input() appStory: IStory;
-  constructor() {}
+
+  constructor(private readonly _modalService: ModalService) {}
 
   ngOnInit() {}
+
+  public openStoryModal(): void {
+    this._modalService.openModal(EditStoryModalComponent, {
+      storyId: this.appStory._id
+    });
+  }
 }
