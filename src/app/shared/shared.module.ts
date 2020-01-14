bf943e2ddfdd2a628ed2562795e2ffcc43179249
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-01-14 23:57:11 +0000

    chore(*): Update relevant modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,32 @@ import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HeaderComponent } from "./layouts/header/header.component";
 import { DragDropModule } from "@angular/cdk/drag-drop";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import {
+  MatDialogModule,
+  MatDialogRef,
+  MatNativeDateModule
+} from "@angular/material";
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, DragDropModule],
-  exports: [FormsModule, ReactiveFormsModule, DragDropModule, HeaderComponent]
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    DragDropModule,
+    MatDialogModule,
+    BrowserAnimationsModule
+  ],
+  exports: [
+    FormsModule,
+    ReactiveFormsModule,
+    DragDropModule,
+    HeaderComponent,
+    MatDialogModule,
+    BrowserAnimationsModule
+  ],
+  providers: [{ provide: MatDialogRef, useValue: {} }]
 })
 export class SharedModule {}
