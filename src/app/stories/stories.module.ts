adf79b5e5fb63ea6480bdd27a999f22399d81db8
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-01-19 20:19:11 +0000

    feat(edit-story-modal): Able to edit title of story from edit story modal

--- src/app/stories/stories.module.ts
+++ src/app/stories/stories.module.ts
@@ -4,6 +4,7 @@ import { StoryComponent } from "./components/story/story.component";
 import { StoryCreateComponent } from "./components/story-create/story-create.component";
 import { SharedModule } from "../shared/shared.module";
 import { EditStoryModalComponent } from "./components/edit-story-modal/edit-story-modal.component";
+import { FormInputTextareaComponent } from "../shared/forms/form-input-textarea/form-input-textarea.component";
 
 @NgModule({
   declarations: [StoryComponent, StoryCreateComponent, EditStoryModalComponent],
