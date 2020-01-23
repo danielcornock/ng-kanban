ad842b03eb38c873f02010bb21e3a7949a0d53c4
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-01-23 22:24:02 +0000

    feat(board-api-service): Update service to set global config

--- src/app/boards/services/board-api/board-api.service.ts
+++ src/app/boards/services/board-api/board-api.service.ts
@@ -2,18 +2,20 @@ import { Injectable } from "@angular/core";
 import { HttpService } from "src/app/shared/api/http-service/http.service";
 import { IBoard } from "../../interfaces/board.interface";
 import { Subject } from "rxjs";
+import { BoardConfigStoreService } from "../board-config-store/board-config-store.service";
 
 @Injectable({
   providedIn: "root"
 })
 export class BoardApiService {
-  public boardTitle: Subject<string> = new Subject<string>();
-
-  constructor(private readonly _httpService: HttpService) {}
+  constructor(
+    private readonly _httpService: HttpService,
+    private readonly _boardConfigStoreService: BoardConfigStoreService
+  ) {}
 
   public async fetchBoard(boardId: string): Promise<IBoard> {
     const { board } = await this._httpService.get(`boards/${boardId}`);
-    this.boardTitle.next(board.title);
+    this._boardConfigStoreService.setConfig(board.config);
     return board;
   }
 }
