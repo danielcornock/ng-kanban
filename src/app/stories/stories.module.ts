fc38fb934ceb214eb468b2e83e2373867703da4d
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-01-24 23:43:21 +0000

    feat(tags-modal): Add tags modal component

--- src/app/stories/stories.module.ts
+++ src/app/stories/stories.module.ts
@@ -5,11 +5,21 @@ import { StoryCreateComponent } from "./components/story-create/story-create.com
 import { SharedModule } from "../shared/shared.module";
 import { EditStoryModalComponent } from "./components/edit-story-modal/edit-story-modal.component";
 import { FormInputTextareaComponent } from "../shared/forms/form-input-textarea/form-input-textarea.component";
+import { TagsModalComponent } from "./components/tags-modal/tags-modal.component";
+import { TagsComponent } from "./components/tags/tags.component";
+import { AddStoryTagsComponent } from './components/add-story-tags/add-story-tags.component';
 
 @NgModule({
-  declarations: [StoryComponent, StoryCreateComponent, EditStoryModalComponent],
+  declarations: [
+    StoryComponent,
+    StoryCreateComponent,
+    EditStoryModalComponent,
+    TagsModalComponent,
+    TagsComponent,
+    AddStoryTagsComponent
+  ],
   imports: [CommonModule, SharedModule],
-  entryComponents: [EditStoryModalComponent],
-  exports: [StoryComponent, StoryCreateComponent]
+  entryComponents: [EditStoryModalComponent, TagsModalComponent],
+  exports: [StoryComponent, StoryCreateComponent, TagsModalComponent]
 })
 export class StoriesModule {}
