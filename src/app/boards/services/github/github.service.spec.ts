bc77b25fec4f31c51915ffb78418b5476f7ef7b9
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-01-26 14:11:22 +0000

    feat(github-service): Add service to consume github api

---
+++ src/app/boards/services/github/github.service.spec.ts
@@ -0,0 +1,73 @@
+import { TestBed } from "@angular/core/testing";
+
+import { GithubService } from "./github.service";
+import { HttpClientStub } from "src/app/shared/api/http-service/testing/http-client.stub";
+import { HttpClient } from "@angular/common/http";
+
+describe("GithubService", () => {
+  let service: GithubService,
+    apiUrl: string,
+    result: any,
+    dependencies: {
+      httpModule: HttpClientStub;
+    };
+
+  function getReturnPromise(type: string) {
+    (dependencies.httpModule[type] as jasmine.Spy).and.returnValue({
+      toPromise: jasmine
+        .createSpy("toPromise")
+        .and.returnValue(`${type}Promise`)
+    });
+  }
+  beforeEach(() => {
+    dependencies = {
+      httpModule: new HttpClientStub()
+    };
+
+    apiUrl = "https://api.github.com";
+
+    TestBed.configureTestingModule({
+      providers: [{ provide: HttpClient, useValue: dependencies.httpModule }]
+    });
+
+    service = TestBed.get(GithubService);
+  });
+
+  it("should be created", () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe("when searching for users ", () => {
+    beforeEach(() => {
+      getReturnPromise("get");
+      result = service.searchUsers("testuser");
+    });
+
+    it("should fetch the users profile", () => {
+      expect(dependencies.httpModule.get).toHaveBeenCalledWith(
+        apiUrl + "/users/testuser"
+      );
+    });
+
+    it("should return the promise", () => {
+      expect(result).toBe("getPromise");
+    });
+  });
+
+  describe("when fetching repos", () => {
+    beforeEach(() => {
+      getReturnPromise("get");
+      result = service.fetchRepos("testuser");
+    });
+
+    it("should fetch the repos", () => {
+      expect(dependencies.httpModule.get).toHaveBeenCalledWith(
+        apiUrl + "/users/testuser/repos"
+      );
+    });
+
+    it("should return the promise", () => {
+      expect(result).toBe("getPromise");
+    });
+  });
+});
---
+++ src/app/boards/services/github/github.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root"
+})
+export class GithubService {
+  private _githubUrl: string = "https://api.github.com";
+
+  constructor(private readonly _httpClient: HttpClient) {}
+
+  public searchUsers(username: string): Promise<any> {
+    return this._httpClient
+      .get(`${this._githubUrl}/users/${username}`)
+      .toPromise();
+  }
+
+  public fetchRepos(username: string): Promise<any> {
+    return this._httpClient
+      .get(`${this._githubUrl}/users/${username}/repos`)
+      .toPromise();
+  }
+}
