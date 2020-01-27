718e1dbbda8e86041f5e0e8f101d2ba2eb54226e
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-01-27 23:54:30 +0000

    feat(form-input-textarea): Abstract logic from textarea to abstract class

--- src/app/shared/forms/form-input-textarea/form-input-textarea.component.ts
+++ src/app/shared/forms/form-input-textarea/form-input-textarea.component.ts
@@ -1,41 +1,9 @@
-import {
-  Component,
-  OnInit,
-  Input,
-  ElementRef,
-  ViewChild,
-  Output,
-  EventEmitter
-} from "@angular/core";
-import { FormGroup } from "@angular/forms";
-import { IControlExport } from "../interfaces/control-export.interface";
+import { Component, OnInit } from "@angular/core";
+import { FormInputComponent } from "../form-input/form-input.component";
 
 @Component({
   selector: "app-form-input-textarea",
   templateUrl: "./form-input-textarea.component.html",
   styleUrls: ["./form-input-textarea.component.scss"]
 })
-export class FormInputTextareaComponent implements OnInit {
-  @Input() public parentForm: FormGroup;
-  @Input() public controlName: string;
-  @Output() public setValue: EventEmitter<IControlExport> = new EventEmitter<
-    IControlExport
-  >();
-
-  @ViewChild("textarea", { static: false })
-  public input: ElementRef;
-
-  constructor() {}
-
-  ngOnInit() {}
-
-  public async onTitleEnter(e: KeyboardEvent) {
-    if (e.keyCode === 13 || e.type === "blur") {
-      this.setValue.emit({
-        value: (e.target as HTMLInputElement).value,
-        name: this.controlName
-      });
-      this.input.nativeElement.blur();
-    }
-  }
-}
+export class FormInputTextareaComponent extends FormInputComponent {}
