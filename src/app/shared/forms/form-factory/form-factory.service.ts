13da2c6e40c1baf078e7ede7a0af56a0cdc9d388
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-01-29 00:52:47 +0000

    feat(form-factory): Refactor existing methods and add from model method (NOT TESTED)

--- src/app/shared/forms/form-factory/form-factory.service.ts
+++ src/app/shared/forms/form-factory/form-factory.service.ts
@@ -1,49 +1,95 @@
 import { Injectable } from "@angular/core";
-import { FormGroup, Validators, ValidatorFn } from "@angular/forms";
+import {
+  FormGroup,
+  Validators,
+  ValidatorFn,
+  FormControl
+} from "@angular/forms";
 import { FormContainer } from "../form-container/form-container";
 import { FormInputField } from "../form-input-field/form-input-field";
-import { IFormInputField } from "../interfaces/form-input-field.interface";
+import { IFormInputFieldConfig } from "../interfaces/form-input-field-config.interface";
 import { IFormConfig } from "../interfaces/form-config.interface";
 import { IAbstractControlDict } from "../interfaces/abstract-control-dict.interface";
 import { ReactiveFormFactory } from "../form-group/reactive-form.factory";
+import { IFormInputFieldGroup } from "../interfaces/form-input-field-group.interface";
+import { IFormInputConfig } from "../interfaces/form-input-config.interface";
+import { IFormCreateManualConfig } from "../interfaces/form-create-manual-config.interface";
 
 @Injectable({
   providedIn: "root"
 })
 export class FormFactory {
   public createForm(formConfig: IFormConfig): FormContainer {
-    const form = new FormContainer();
-    form.form = this._createFormGroup(formConfig.fields);
-
-    form.fields = formConfig.fields;
-
-    return form;
+    return new FormContainer(
+      this._createFieldGroup(formConfig.fields),
+      this._createFormGroup(formConfig.fields)
+    );
   }
 
-  public createInput(inputConfig: IFormInputField): FormInputField {
-    const control = ReactiveFormFactory.createFormControl(
-      inputConfig.config.getValue()
-    );
-
-    control.setValidators(this._createValidators(inputConfig));
-
-    const formInputField = new FormInputField(
-      control,
+  public createInput(inputConfig: IFormInputFieldConfig): FormInputField {
+    return new FormInputField(
+      this._createControl(inputConfig.config),
       inputConfig.name,
       inputConfig.config
     );
-
-    return formInputField;
   }
 
-  private _createValidators(inputConfig: IFormInputField) {
+  public createModelForm<T>(model: T, formConfig: IFormCreateManualConfig) {
+    const formInputArray: Array<FormInputField> = [];
+
+    formConfig.fields.forEach(item => {
+      this._setModelGetterFn<T>(item, model);
+      this._setModelSetterFn<T>(item, model);
+
+      formInputArray.push(this.createInput(item));
+    });
+
+    return this.createForm({ fields: formInputArray });
+  }
+
+  private _createControl(inputConfigConfig: IFormInputConfig): FormControl {
+    const control = ReactiveFormFactory.createFormControl(
+      inputConfigConfig.getValue()
+    );
+
+    control.setValidators(this._createValidators(inputConfigConfig));
+
+    return control;
+  }
+
+  private _setModelGetterFn<T>(fieldConfig: IFormInputFieldConfig, model: T) {
+    if (!fieldConfig.config.getValue) {
+      fieldConfig.config.getValue = () => model[fieldConfig.name];
+    }
+  }
+
+  private _setModelSetterFn<T>(fieldConfig: IFormInputFieldConfig, model: T) {
+    if (!fieldConfig.config.setValue) {
+      fieldConfig.config.setValue = (val: any) => {
+        model[fieldConfig.name] = val;
+      };
+    }
+  }
+
+  private _createValidators(inputConfig: IFormInputConfig) {
     const validators: Array<ValidatorFn> = [];
 
-    if (inputConfig.config.required) {
+    if (inputConfig.required) {
       validators.push(Validators.required);
     }
 
     return validators;
+  }
+
+  private _createFieldGroup(
+    inputFieldsArray: Array<IFormInputFieldConfig>
+  ): IFormInputFieldGroup {
+    const fieldObj = {};
+    inputFieldsArray.forEach(field => {
+      fieldObj[field.name] = field;
+    });
+
+    return fieldObj;
   }
 
   private _createFormGroup(fields: Array<FormInputField>): FormGroup {
