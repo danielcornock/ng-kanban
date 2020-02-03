a55d0d701ccd2072962306aa97e1b499192cef48
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-03 22:56:16 +0000

    fix(board-api): Remove redundant imports

--- src/app/boards/services/board-api/board-api.service.ts
+++ src/app/boards/services/board-api/board-api.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpService } from "src/app/shared/api/http-service/http.service";
 import { IBoard } from "../../interfaces/board.interface";
-import { Subject } from "rxjs";
 import { BoardConfigStoreService } from "../board-config-store/board-config-store.service";
 
 @Injectable({
