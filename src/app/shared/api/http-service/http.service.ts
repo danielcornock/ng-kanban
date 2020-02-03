65973861dbc9298cc3c8205ac289a663a557dfec
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-03 18:38:31 +0000

    fix(http-service): Return http service to base state

--- src/app/shared/api/http-service/http.service.ts
+++ src/app/shared/api/http-service/http.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { HttpModel } from "../http-model/http-model";
 
 @Injectable({
   providedIn: "root"
