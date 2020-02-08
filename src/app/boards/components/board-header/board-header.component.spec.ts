7d7d28e8f5b56cccedcf2fb147c93fc50c3f146b
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-08 17:20:49 +0000

    feat(board-header): Listen to modal response on close

--- src/app/boards/components/board-header/board-header.component.spec.ts
+++ src/app/boards/components/board-header/board-header.component.spec.ts
@@ -9,6 +9,10 @@ import { ModalService } from "src/app/shared/modal/modal-service/modal.service";
 import { IconComponentStub } from "../../../icons/icon/icon.component.stub";
 import { GetElement } from "src/app/testing/get-element/get-element";
 import { GithubConfigModalComponent } from "../github-config-modal/github-config-modal.component";
+import { BoardConfigStoreServiceStub } from "../../services/board-config-store/board-config-store.service.stub";
+import { BoardConfigStoreService } from "../../services/board-config-store/board-config-store.service";
+import { Subject } from "rxjs";
+import { HttpModelStub } from "src/app/shared/api/http-model/http-model.stub";
 
 describe("BoardHeaderComponent", () => {
   let component: BoardHeaderComponent,
@@ -16,17 +20,23 @@ describe("BoardHeaderComponent", () => {
     getElement: GetElement<BoardHeaderComponent>,
     dependencies: {
       modalService: ModalServiceStub;
+      boardConfigStoreService: BoardConfigStoreServiceStub;
     };
 
   beforeEach(async(() => {
     dependencies = {
-      modalService: new ModalServiceStub()
+      modalService: new ModalServiceStub(),
+      boardConfigStoreService: new BoardConfigStoreServiceStub()
     };
 
     TestBed.configureTestingModule({
       declarations: [BoardHeaderComponent, IconComponentStub],
       providers: [
-        { provide: ModalService, useValue: dependencies.modalService }
+        { provide: ModalService, useValue: dependencies.modalService },
+        {
+          provide: BoardConfigStoreService,
+          useValue: dependencies.boardConfigStoreService
+        }
       ]
     }).compileComponents();
   }));
@@ -35,6 +45,13 @@ describe("BoardHeaderComponent", () => {
     fixture = TestBed.createComponent(BoardHeaderComponent);
     component = fixture.componentInstance;
     component.appBoardHeaderTitle = "Test board";
+
+    const boardModel = new HttpModelStub();
+    boardModel.data.config = {
+      repos: "hi"
+    };
+
+    component.appBoardHeaderBoardModel = boardModel;
     getElement = new GetElement(fixture, "boardHeader");
   });
 
@@ -63,15 +80,37 @@ describe("BoardHeaderComponent", () => {
     });
 
     describe("when the github button is clicked", () => {
+      let onCloseSubject: Subject<any>;
+
       beforeEach(() => {
+        onCloseSubject = new Subject();
+        (dependencies.modalService.openModal as jasmine.Spy).and.returnValue({
+          afterClosed: jasmine
+            .createSpy()
+            .and.returnValue(onCloseSubject.asObservable())
+        });
         getElement.byCss("github").nativeElement.click();
       });
 
       it("should open the github modal", () => {
         expect(dependencies.modalService.openModal).toHaveBeenCalledWith(
           GithubConfigModalComponent,
-          {}
+          {
+            repos: component.appBoardHeaderBoardModel.data.config.repos
+          }
         );
+      });
+
+      describe("when the modal is closed", () => {
+        beforeEach(() => {
+          onCloseSubject.next({ selectedRepos: "newRepos" });
+        });
+
+        it("should save the config to the board", () => {
+          expect(
+            dependencies.boardConfigStoreService.saveConfig
+          ).toHaveBeenCalledWith({ repos: "newRepos" });
+        });
       });
     });
   });
--- src/app/boards/components/board-header/board-header.component.ts
+++ src/app/boards/components/board-header/board-header.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Input } from "@angular/core";
 import { ModalService } from "src/app/shared/modal/modal-service/modal.service";
 import { TagsModalComponent } from "../../../stories/components/tags-modal/tags-modal.component";
 import { GithubConfigModalComponent } from "../github-config-modal/github-config-modal.component";
+import { IHttpModel } from "src/app/shared/api/http-model/http-model.interface";
+import { IGithubConfig } from "./interfaces/github-config.interface";
+import { BoardConfigStoreService } from "../../services/board-config-store/board-config-store.service";
 
 @Component({
   selector: "app-board-header",
@@ -10,8 +13,12 @@ import { GithubConfigModalComponent } from "../github-config-modal/github-config
 })
 export class BoardHeaderComponent implements OnInit {
   @Input() appBoardHeaderTitle: string;
+  @Input() appBoardHeaderBoardModel: IHttpModel;
 
-  constructor(private readonly _modalService: ModalService) {}
+  constructor(
+    private readonly _modalService: ModalService,
+    private readonly _boardConfigStoreService: BoardConfigStoreService
+  ) {}
 
   ngOnInit() {}
 
@@ -20,6 +27,24 @@ export class BoardHeaderComponent implements OnInit {
   }
 
   public openGithubModal(): void {
-    this._modalService.openModal(GithubConfigModalComponent, {});
+    console.log(this.appBoardHeaderBoardModel);
+    const modalClose = this._modalService.openModal(
+      GithubConfigModalComponent,
+      {
+        repos: this.appBoardHeaderBoardModel.data.config.repos
+      }
+    );
+
+    modalClose.afterClosed().subscribe(data => {
+      this._saveConfigToBoard(data);
+    });
+  }
+
+  private _saveConfigToBoard(data: IGithubConfig): void {
+    this.appBoardHeaderBoardModel.data.config.repos = data.selectedRepos;
+
+    this._boardConfigStoreService.saveConfig(
+      this.appBoardHeaderBoardModel.data.config
+    );
   }
 }
