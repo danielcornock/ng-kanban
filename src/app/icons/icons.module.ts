68886ec69274830cb2c6474b91a114c129881aaf
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-09 17:07:16 +0000

    feat(icons): Add icons

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FeatherModule } from "angular-feather";
-import { Github } from "angular-feather/icons";
+import { Github, GitCommit, Plus, XCircle } from "angular-feather/icons";
 import { IconComponent } from "./icon/icon.component";
 
-const icons = { Github };
+const icons = { Github, GitCommit, Plus, XCircle };
 @NgModule({
   imports: [FeatherModule.pick(icons)],
   exports: [FeatherModule, IconComponent],
