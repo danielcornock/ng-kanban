81d09a1222ddc5f7dad89f0232ee6136e142f107
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-12 23:20:13 +0000

    make app header only appear when logged in

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,13 @@
-import { TestBed, async, ComponentFixture } from "@angular/core/testing";
-import { RouterTestingModule } from "@angular/router/testing";
-import { AppComponent } from "./app.component";
-import { AuthService } from "./authentication/services/auth/auth.service";
-import { AuthServiceStub } from "./authentication/services/auth/auth.service.stub";
-import { HeaderComponentStub } from "./shared/layouts/header/header.component.stub";
-import { DebugElement } from "@angular/core";
-import { By } from "@angular/platform-browser";
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { AuthService } from './authentication/services/auth/auth.service';
+import { AuthServiceStub } from './authentication/services/auth/auth.service.stub';
+import { HeaderComponentStub } from './shared/layouts/header/header.component.stub';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
-describe("AppComponent", () => {
+describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>,
     dependencies: {
       authService: AuthServiceStub;
@@ -32,17 +32,7 @@ describe("AppComponent", () => {
     fixture = TestBed.createComponent(AppComponent);
   });
 
-  describe("on initialisation", () => {
-    beforeEach(() => {
-      fixture.detectChanges();
-    });
-
-    it("should check if the user is authenticated", () => {
-      expect(dependencies.authService.isAuthenticated).toHaveBeenCalledWith();
-    });
-  });
-
-  describe("when the user is not authenticated", () => {
+  describe('when the user is not authenticated', () => {
     beforeEach(() => {
       (dependencies.authService.isAuthenticated as jasmine.Spy).and.returnValue(
         false
@@ -51,12 +41,12 @@ describe("AppComponent", () => {
       fixture.detectChanges();
     });
 
-    it("should not display the header", () => {
+    it('should not display the header', () => {
       expect(getHeader() === null).toBe(true);
     });
   });
 
-  describe("when the user is authenticated", () => {
+  describe('when the user is authenticated', () => {
     beforeEach(() => {
       (dependencies.authService.isAuthenticated as jasmine.Spy).and.returnValue(
         true
@@ -64,7 +54,7 @@ describe("AppComponent", () => {
       fixture.detectChanges();
     });
 
-    it("should display the header", () => {
+    it('should display the header', () => {
       expect(getHeader() === null).toBe(false);
     });
   });
