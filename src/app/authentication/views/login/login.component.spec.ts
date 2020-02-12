9013b12d4e67fef608dd7ae2252cdae2ab1e05f8
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-12 23:46:33 +0000

    make login component use form framework and add styling

--- src/app/authentication/views/login/login.component.spec.ts
+++ src/app/authentication/views/login/login.component.spec.ts
@@ -4,49 +4,62 @@ import {
   TestBed,
   fakeAsync,
   tick
-} from "@angular/core/testing";
+} from '@angular/core/testing';
 
-import { LoginComponent } from "./login.component";
-import { FormBuilder } from "@angular/forms";
-import { HttpService } from "src/app/shared/api/http-service/http.service";
-import { HttpServiceStub } from "src/app/shared/api/http-service/http.service.stub";
-import { RouterService } from "src/app/shared/router/router.service";
-import { RouterServiceStub } from "src/app/shared/router/router.service.stub";
-import { AuthService } from "../../services/auth/auth.service";
-import { AuthServiceStub } from "../../services/auth/auth.service.stub";
-import { SharedModule } from "src/app/shared/shared.module";
-import { DebugElement } from "@angular/core";
-import { By } from "@angular/platform-browser";
-import { Subject } from "rxjs";
-import { TestPromise } from "src/app/testing/test-promise/test-promise";
+import { LoginComponent } from './login.component';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { HttpService } from 'src/app/shared/api/http-service/http.service';
+import { HttpServiceStub } from 'src/app/shared/api/http-service/http.service.stub';
+import { RouterService } from 'src/app/shared/router/router.service';
+import { RouterServiceStub } from 'src/app/shared/router/router.service.stub';
+import { AuthService } from '../../services/auth/auth.service';
+import { AuthServiceStub } from '../../services/auth/auth.service.stub';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { TestPromise } from 'src/app/testing/test-promise/test-promise';
+import { FormFactory } from 'src/app/shared/forms/services/form-factory/form-factory.service';
+import { FormFactoryStub } from 'src/app/shared/forms/services/form-factory/form-factory.service.stub';
+import { formInputTypes } from 'src/app/shared/forms/constants/form-input-types';
+import { FormContainerStub } from 'src/app/shared/forms/form-container/form-container.stub';
+import { FormInputTextComponentStub } from 'src/app/shared/forms/form-input-text/form-input-text.component.stub';
+import { FormInputFieldStub } from 'src/app/shared/forms/form-input-field/form-input-field.stub';
+import { FormInputField } from 'src/app/shared/forms/form-input-field/form-input-field';
 
-describe("LoginComponent", () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
-  let dependencies: {
-    formBuilder: FormBuilder;
-    httpService: HttpServiceStub;
-    routerService: RouterServiceStub;
-    authService: AuthServiceStub;
-  };
+describe('LoginComponent', () => {
+  let component: LoginComponent,
+    fixture: ComponentFixture<LoginComponent>,
+    loginForm: FormContainerStub,
+    dependencies: {
+      formFactory: FormFactoryStub;
+      httpService: HttpServiceStub;
+      routerService: RouterServiceStub;
+      authService: AuthServiceStub;
+    };
 
   function getSubmitButton(): DebugElement {
-    return fixture.debugElement.query(By.css(".login-submit"));
+    return fixture.debugElement.query(By.css('.login-submit'));
+  }
+
+  function getFormFields(): Array<DebugElement> {
+    return fixture.debugElement.queryAll(
+      By.directive(FormInputTextComponentStub)
+    );
   }
 
   beforeEach(async(async () => {
     dependencies = {
-      formBuilder: new FormBuilder(),
+      formFactory: new FormFactoryStub(),
       httpService: new HttpServiceStub(),
       routerService: new RouterServiceStub(),
       authService: new AuthServiceStub()
     };
 
     await TestBed.configureTestingModule({
-      imports: [SharedModule],
-      declarations: [LoginComponent],
+      imports: [ReactiveFormsModule],
+      declarations: [LoginComponent, FormInputTextComponentStub],
       providers: [
-        { provide: FormBuilder, useValue: dependencies.formBuilder },
+        { provide: FormFactory, useValue: dependencies.formFactory },
         { provide: HttpService, useValue: dependencies.httpService },
         { provide: RouterService, useValue: dependencies.routerService },
         { provide: AuthService, useValue: dependencies.authService }
@@ -58,67 +71,104 @@ describe("LoginComponent", () => {
 
   beforeEach(() => {
     component = fixture.componentInstance;
-    const loginForm = dependencies.formBuilder.group({
-      email: [""],
-      password: [""]
-    });
-    spyOn(dependencies.formBuilder, "group").and.returnValue(loginForm);
-    fixture.detectChanges();
+    loginForm = new FormContainerStub();
+    loginForm.fields = {
+      email: ('email' as unknown) as FormInputField,
+      password: ('password' as unknown) as FormInputField
+    };
+
+    (dependencies.formFactory.createForm as jasmine.Spy).and.returnValue(
+      loginForm
+    );
   });
 
-  describe("on initialisation", () => {
-    beforeEach(() => {});
+  describe('on initialisation', () => {
+    beforeEach(() => {
+      fixture.detectChanges();
+    });
 
-    it("should create the form", () => {
-      expect(dependencies.formBuilder.group).toHaveBeenCalledWith({
-        email: ["", [jasmine.any(Function), jasmine.any(Function)]],
-        password: ["", jasmine.any(Function)]
+    it('should create the form', () => {
+      expect(dependencies.formFactory.createForm).toHaveBeenCalledWith({
+        fields: [
+          {
+            name: 'email',
+            type: formInputTypes.EMAIL,
+            config: {
+              required: true,
+              customValidators: [jasmine.any(Function)],
+              setValue: jasmine.any(Function)
+            }
+          },
+          {
+            name: 'password',
+            type: formInputTypes.PASSWORD,
+            config: {
+              required: true,
+              setValue: jasmine.any(Function)
+            }
+          }
+        ]
       });
     });
 
-    describe("when the user submits the form", () => {
-      let postPromise: TestPromise<any>;
+    it('should display the email field', () => {
+      expect(getFormFields()[0].componentInstance.fieldConfig).toBe('email');
+      expect(getFormFields()[1].componentInstance.fieldConfig).toBe('password');
+    });
 
+    describe('when the user sets the values for the email and password', () => {
       beforeEach(() => {
-        component.loginForm.value.email = "daniel@me.com";
-        postPromise = new TestPromise();
-
-        (dependencies.httpService.post as jasmine.Spy).and.returnValue(
-          postPromise.promise
-        );
-
-        getSubmitButton().nativeElement.click();
+        (dependencies.formFactory.createForm as jasmine.Spy).calls
+          .argsFor(0)[0]
+          .fields[0].config.setValue({ name: 'email', value: 'dan@me.com' });
+        (dependencies.formFactory.createForm as jasmine.Spy).calls
+          .argsFor(0)[0]
+          .fields[1].config.setValue({ name: 'password', value: 'password' });
       });
 
-      it("should post the results to the API", () => {
-        expect(dependencies.httpService.post).toHaveBeenCalledWith(
-          "auth/login",
-          {
-            email: "daniel@me.com",
-            password: ""
-          }
-        );
-      });
+      describe('when the user submits the form', () => {
+        let postPromise: TestPromise<any>;
 
-      describe("when the user logs in successfully", () => {
-        beforeEach(fakeAsync(() => {
-          postPromise.resolve({ jwt: "test-jwt" });
+        beforeEach(() => {
+          postPromise = new TestPromise();
 
-          tick();
+          (dependencies.httpService.post as jasmine.Spy).and.returnValue(
+            postPromise.promise
+          );
 
-          fixture.detectChanges();
-        }));
+          getSubmitButton().nativeElement.click();
+        });
 
-        it("should set the jwt to the return", () => {
-          expect(dependencies.authService.setJwt).toHaveBeenCalledWith(
-            "test-jwt"
+        it('should post the results to the API', () => {
+          expect(dependencies.httpService.post).toHaveBeenCalledWith(
+            'auth/login',
+            {
+              email: 'dan@me.com',
+              password: 'password'
+            }
           );
         });
 
-        it("should navigate the user to the home page", () => {
-          expect(dependencies.routerService.navigate).toHaveBeenCalledWith(
-            "home"
-          );
+        describe('when the user logs in successfully', () => {
+          beforeEach(fakeAsync(() => {
+            postPromise.resolve({ jwt: 'test-jwt' });
+
+            tick();
+
+            fixture.detectChanges();
+          }));
+
+          it('should set the jwt to the return', () => {
+            expect(dependencies.authService.setJwt).toHaveBeenCalledWith(
+              'test-jwt'
+            );
+          });
+
+          it('should navigate the user to the home page', () => {
+            expect(dependencies.routerService.navigate).toHaveBeenCalledWith(
+              'home'
+            );
+          });
         });
       });
     });
--- src/app/authentication/views/login/login.component.ts
+++ src/app/authentication/views/login/login.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, Validators } from '@angular/forms';
 import { HttpService } from 'src/app/shared/api/http-service/http.service';
 import { RouterService } from 'src/app/shared/router/router.service';
 import { AuthService } from '../../services/auth/auth.service';
+import { FormFactory } from 'src/app/shared/forms/services/form-factory/form-factory.service';
+import { FormContainer } from 'src/app/shared/forms/form-container/form-container';
+import { IControlExport } from 'src/app/shared/forms/interfaces/control-export.interface';
+import { IUserLoginCredentials } from '../../interfaces/user-login-credentials.interface';
+import { formInputTypes } from 'src/app/shared/forms/constants/form-input-types';
 
 @Component({
   selector: 'app-login',
@@ -10,40 +15,47 @@ import { AuthService } from '../../services/auth/auth.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  public loginForm: FormGroup;
-
-  private readonly _formBuilder: FormBuilder;
-  private readonly _httpService: HttpService;
-  private readonly _router: RouterService;
-  private readonly _authService: AuthService;
+  public formContainer: FormContainer;
+  private _userCredentials: IUserLoginCredentials;
 
   constructor(
-    formBuilder: FormBuilder,
-    httpService: HttpService,
-    router: RouterService,
-    authService: AuthService
-  ) {
-    this._formBuilder = formBuilder;
-    this._httpService = httpService;
-    this._router = router;
-    this._authService = authService;
-  }
+    private readonly _httpService: HttpService,
+    private readonly _router: RouterService,
+    private readonly _authService: AuthService,
+    private readonly _formFactory: FormFactory
+  ) {}
 
   public ngOnInit(): void {
-    this.loginForm = this._formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
+    this._userCredentials = {} as IUserLoginCredentials;
+    this.formContainer = this._formFactory.createForm({
+      fields: [
+        {
+          name: 'email',
+          type: formInputTypes.EMAIL,
+          config: {
+            required: true,
+            customValidators: [Validators.email],
+            setValue: (result: IControlExport) => {
+              this._userCredentials.email = result.value;
+            }
+          }
+        },
+        {
+          name: 'password',
+          type: formInputTypes.PASSWORD,
+          config: {
+            required: true,
+            setValue: (result: IControlExport) => {
+              this._userCredentials.password = result.value;
+            }
+          }
+        }
+      ]
     });
   }
 
   public login(): void {
-    if (!this.loginForm.valid) {
-      // TODO Add pop up message
-      console.error('Form Not Valid!');
-      return;
-    }
-
-    this._httpService.post('auth/login', this.loginForm.value).then(res => {
+    this._httpService.post('auth/login', this._userCredentials).then(res => {
       this._authService.setJwt(res.jwt);
       this._router.navigate('home');
     });
