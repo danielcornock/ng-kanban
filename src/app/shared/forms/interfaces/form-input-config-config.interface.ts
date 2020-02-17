e42619e7b5a3a4e9982696f7473451c9e379e065
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-17 19:38:59 +0000

    add custom validators to form input config config interface

--- src/app/shared/forms/interfaces/form-input-config-config.interface.ts
+++ src/app/shared/forms/interfaces/form-input-config-config.interface.ts
@@ -1,7 +1,9 @@
-import { IControlExport } from "./control-export.interface";
+import { IControlExport } from './control-export.interface';
+import { ValidatorFn } from '@angular/forms';
 
 export interface IFormInputConfigConfig {
   required?: boolean;
   getValue?: () => any;
   setValue?: (value: IControlExport) => void;
+  customValidators?: Array<ValidatorFn>;
 }
