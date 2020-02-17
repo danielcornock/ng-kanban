b9d191a69415b455cc9bc7f15209f52aa355f11b
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-17 19:26:22 +0000

    add components to module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HeaderComponent } from "./layouts/header/header.component";
-import { DragDropModule } from "@angular/cdk/drag-drop";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './layouts/header/header.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatDialogModule, MatDialogRef } from "@angular/material";
-import { FormInputTextareaComponent } from "./forms/form-input-textarea/form-input-textarea.component";
-import { TextAreaAutoSizeDirective } from "./forms/directives/text-area-auto-size.directive";
-import { IconsModule } from "../icons/icons.module";
+import { MatDialogModule, MatDialogRef } from '@angular/material';
+import { FormInputTextareaComponent } from './forms/form-input-textarea/form-input-textarea.component';
+import { TextAreaAutoSizeDirective } from './forms/directives/text-area-auto-size.directive';
+import { IconsModule } from '../icons/icons.module';
+import { FormInputTextComponent } from './forms/form-input-text/form-input-text.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     FormInputTextareaComponent,
-    TextAreaAutoSizeDirective
+    TextAreaAutoSizeDirective,
+    FormInputTextComponent
   ],
   imports: [
     CommonModule,
@@ -33,7 +35,8 @@ import { IconsModule } from "../icons/icons.module";
     MatDialogModule,
     BrowserAnimationsModule,
     FormInputTextareaComponent,
-    IconsModule
+    IconsModule,
+    FormInputTextComponent
   ],
   providers: [{ provide: MatDialogRef, useValue: {} }]
 })
