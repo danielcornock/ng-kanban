127bb02e4f9ed1689d277fa2b0e3bde147bad720
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-17 19:47:50 +0000

    add validation to form input field

--- src/app/shared/forms/form-input-field/form-input-field.spec.ts
+++ src/app/shared/forms/form-input-field/form-input-field.spec.ts
@@ -1,30 +1,31 @@
-import { FormInputField } from "./form-input-field";
-import { IFormInputConfig } from "../interfaces/form-input-config.interface";
-import { ReactiveFormFactory } from "../form-group/reactive-form.factory";
-import { FormControl, Validators } from "@angular/forms";
+import { FormInputField } from './form-input-field';
+import { IFormInputConfig } from '../interfaces/form-input-config.interface';
+import { ReactiveFormFactory } from '../services/form-group/reactive-form.factory';
+import { FormControl, Validators } from '@angular/forms';
 
-describe("FormInputField", () => {
+describe('FormInputField', () => {
   let formInputField: FormInputField,
     inputConfig: IFormInputConfig,
     inputControl: FormControl;
 
   beforeEach(() => {
     inputConfig = {
-      name: "input-name",
+      name: 'input-name',
       config: {
         required: true,
-        getValue: jasmine.createSpy("getValue").and.returnValue(12),
-        setValue: jasmine.createSpy("setValue")
+        getValue: jasmine.createSpy('getValue').and.returnValue(12),
+        setValue: jasmine.createSpy('setValue'),
+        customValidators: [jasmine.createSpy('customVal')]
       }
     };
 
     inputControl = new FormControl(12);
-    spyOn(inputControl, "setValidators");
+    spyOn(inputControl, 'setValidators');
   });
 
-  describe("when creating", () => {
+  describe('when creating', () => {
     beforeEach(() => {
-      spyOn(ReactiveFormFactory, "createFormControl");
+      spyOn(ReactiveFormFactory, 'createFormControl');
 
       (ReactiveFormFactory.createFormControl as jasmine.Spy).and.returnValue(
         inputControl
@@ -32,23 +33,24 @@ describe("FormInputField", () => {
       formInputField = FormInputField.create(inputConfig);
     });
 
-    it("should get the value of the field", () => {
+    it('should get the value of the field', () => {
       expect(inputConfig.config.getValue).toHaveBeenCalledWith();
     });
 
-    it("should create the angular form control", () => {
+    it('should create the angular form control', () => {
       expect(ReactiveFormFactory.createFormControl).toHaveBeenCalledWith(12);
     });
 
-    it("should create the validators", () => {
+    it('should create the validators', () => {
       expect(inputControl.setValidators).toHaveBeenCalledWith([
-        Validators.required
+        Validators.required,
+        inputConfig.config.customValidators[0]
       ]);
     });
 
-    it("should create the form input field", () => {
+    it('should create the form input field', () => {
       expect(formInputField instanceof FormInputField);
-      expect(formInputField.name).toBe("input-name");
+      expect(formInputField.name).toBe('input-name');
       expect(formInputField.config).toBe(inputConfig.config);
       expect(formInputField.control).toBe(inputControl);
     });
